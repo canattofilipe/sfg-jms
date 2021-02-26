f480442537ba132df9c9f6baee46acd393fcfaf9
canattofilipe <canatto.filipe@gmail.com>
2021-02-26 08:58:47 -0300

    sending messages

--- src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
+++ src/main/java/guru/springframework/sfgjms/model/HelloWorldMessage.java
@@ -17,5 +17,5 @@ public class HelloWorldMessage implements Serializable {
   static final long serialVersionUID = -6703826490277916847L;
 
   private UUID id;
-  private String Message;
+  private String message;
 }
